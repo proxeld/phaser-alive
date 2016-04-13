4f72a465e388d940c69a4abd2a7a1f6fae370abc
Jakub Pelczar <kuba.pelczar@gmail.com>
2016-04-13 15:30:25 +0200

    TODO code removed

--- src/js/game.js
+++ src/js/game.js
@@ -57,17 +57,13 @@ var Loading = {
         game.load.image('menuBg', config.IMG_PATH + 'menuBg.png');
 
         ////////////////////////////////////////
-        // TODO: n. Load box image
-        game.load.image('box', config.IMG_PATH + 'box.png');
+        // TODO: 8. Load box image
 
-        // loading spritesheet
         ////////////////////////////
-        // TODO: 0. Load spritesheet
-        game.load.spritesheet('link', config.SPRITESHEETS_PATH + 'link.png', 90, 90);
+        // TODO: 1. Load spritesheet
 
         ////////////////////////////////////////
-        // TODO: n. Load sound
-        game.load.audio('open', config.SOUNDSS_PATH + 'open.mp3');
+        // TODO: 14. Load sound
 
         // start Phaser loading routine
         game.load.start();
@@ -107,89 +103,50 @@ var Gameplay = {
 
 
         ////////////////////////////////////////
-        // TODO: 1. Add spritesheet to the game
-        this.linkSprite = game.add.sprite(game.width / 2 - 50, 300, 'link');
-
-        // enable collision with tiles
-        game.physics.enable(this.linkSprite, Phaser.Physics.ARCADE);
+        // TODO: 2. Add spritesheet to the game
 
         ////////////////////////////////////////
-        // TODO: n. Set size of link body, a.k.a. collision area
-        this.linkSprite.body.setSize(28, 54, 28, 18);
+        // TODO: 10. Enable collision for player sprite
 
         ////////////////////////////////////////
-        // TODO: n. Add box sprite to the game
-        this.boxSprite = game.add.sprite(100, game.height/2 - 50, 'box');
-        game.physics.enable(this.boxSprite, Phaser.Physics.ARCADE);
-        this.boxSprite.body.immovable = true;
+        // TODO: 13. Set size of link body, a.k.a. collision area
 
         ////////////////////////////////////////
-        // TODO: n. Add character animations
-        this.linkSprite.animations.add('walkLeft', [0, 1, 2, 3, 4]);
-        this.linkSprite.animations.add('walkRight', [5, 6, 7, 8, 9]);
-        this.linkSprite.animations.add('walkUp', [10, 11, 12, 13, 14]);
-        this.linkSprite.animations.add('walkDown', [15, 16, 17, 18, 19]);
+        // TODO: 9. Add box sprite to the game
 
         ////////////////////////////////////////
-        // TODO: n. Create keys for arrows
-        this.leftKey = game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
-        this.rightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
-        this.upKey = game.input.keyboard.addKey(Phaser.Keyboard.UP);
-        this.downKey = game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+        // TODO: 10. Enable collision for box sprite
 
         ////////////////////////////////////////
-        // TODO: n. Add sound
-        this.openSound = game.add.audio('open');
+        // TODO: 12. Make box immovable
+
+        ////////////////////////////////////////
+        // TODO: 6. Add character animations
+
+        ////////////////////////////////////////
+        // TODO: 3. Create keys for arrows
+
+        ////////////////////////////////////////
+        // TODO: 15. Add sound
 
     },
     update: function () {
         //////////////////////////////////////////////////
-        // TODO: n. Set zero velocity for each update call
-        this.linkSprite.body.velocity.x = 0;
-        this.linkSprite.body.velocity.y = 0;
+        // TODO: 5. Set zero velocity for each update call
 
         ////////////////////////////////////////
-        // TODO: n. React on arrows keys pressed
-        if (this.leftKey.isDown) {
-            this.linkSprite.body.velocity.x = -150;
-
-            ////////////////////////////////////////
-            // TODO: n. Play animation
-            this.linkSprite.animations.play('walkLeft', 6);
-        }
-        else if (this.rightKey.isDown) {
-            this.linkSprite.body.velocity.x = 150;
-
-            ////////////////////////////////////////
-            // TODO: n. Play animation
-            this.linkSprite.animations.play('walkRight', 6);
-        }
-        else if (this.upKey.isDown) {
-            this.linkSprite.body.velocity.y = -150;
-
-            ////////////////////////////////////////
-            // TODO: n. Play animation
-            this.linkSprite.animations.play('walkUp', 6);
-        }
-        else if (this.downKey.isDown) {
-            this.linkSprite.body.velocity.y = 150;
-
-            ////////////////////////////////////////
-            // TODO: n. Play animation
-            this.linkSprite.animations.play('walkDown', 6);
-        }
+        // TODO: 4. React on arrows keys pressed
+        // TODO: 7. Play animation
 
         ////////////////////////////////////////
-        // TODO: n. Perform collision check
-        // TODO: n. Play 'open' sound after collision
-        game.physics.arcade.collide(this.linkSprite, this.boxSprite, this.playSound, null, this);
+        // TODO: 11. Perform collision check
+        // TODO: 17. Play 'open' sound after collision
     },
     goToMainMenu: function () {
         game.state.start('Menu');
     },
     playSound: function () {
         ////////////////////////////////////////
-        // TODO: n. Play sound
-        this.openSound.play();
+        // TODO: 16. Play sound
     }
 };
